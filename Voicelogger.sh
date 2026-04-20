bdad1141e5d138d7a4a7dd38ffa3dfb35b20f47b
naveen9442 <116275022+naveen9442@users.noreply.github.com>
2026-04-20 10:11:27 +0530

    My sql data base dump voicelogger
    This script automates the backup of the Voicelogs database using mysqldump and compresses the output.

---
+++ Voicelogger.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+################################################################
+##
+##   MySQL Voicelogger Database Backup Script
+##
+##
+##  Script Release Date : Jan 5th, 2022
+##
+################################################################
+
+ 
+export PATH=/bin:/usr/bin:/usr/local/bin
+TODAY=`date +"%d%b%Y"`
+ 
+################################################################
+################## Update below values  ########################
+
+DB_BACKUP_PATH='/home/Haloocom/HaloocomHealthCheck/dbbackup'
+MYSQL_HOST='localhost'
+MYSQL_PORT='3306'
+MYSQL_USER='root'
+MYSQL_PASSWORD='Hal0o(0m@72427242'
+DATABASE_NAME='Voicelogs'
+
+#################################################################
+
+ 
+mkdir -p ${DB_BACKUP_PATH}/${TODAY}
+echo "Backup started for database - ${DATABASE_NAME}"
+ 
+ 
+mysqldump -h ${MYSQL_HOST} \
+   -P ${MYSQL_PORT} \
+   -u ${MYSQL_USER} \
+   -p${MYSQL_PASSWORD} \
+   ${DATABASE_NAME} | gzip > ${DB_BACKUP_PATH}/${TODAY}/${DATABASE_NAME}-${TODAY}.sql.gz
+ 
+if [ $? -eq 0 ]; then
+  echo "Database backup successfully completed"
+else
+  echo "Error found during backup"
+  exit 1
+fi
